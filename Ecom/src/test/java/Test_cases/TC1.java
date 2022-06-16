047aafda75a0425844e033b1a1d88b4a481cfcb7
macos <macos@localhost>
2022-06-16 17:14:49 +0530

    commit by indu

---
+++ Ecom/src/test/java/Test_cases/TC1.java
@@ -0,0 +1,42 @@
+package Test_cases;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pageobjects.Login;
+public class TC1 {
+	
+	
+	static WebDriver driver;
+	
+	@BeforeMethod
+	public void launchbrowser() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/macos/Desktop/drivers/chromedriver");
+		driver= new ChromeDriver();
+		driver.get("https://marketplace-test.ordofy.com:4500/home");
+		driver.manage().window().maximize();
+		System.out.println("ordofy launched");
+		Thread.sleep(10000);
+	}
+	
+	
+	@Test
+	public void launch() throws InterruptedException {
+		Login b = new Login(driver);
+		b.Profile_click();
+		Thread.sleep(5000);
+		b.Send_username();
+	
+	}
+	
+	
+	
+	
+	}
+	
+
+
