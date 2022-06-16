d4ccec2e464aba9f42ac035c9a4e30b69ee9e694
macos <macos@localhost>
2022-06-16 12:43:15 +0530

    commit by indu

---
+++ Ecom/src/test/java/pageobjects/Login.java
@@ -0,0 +1,43 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	WebDriver driver;
+	
+	public Login(WebDriver driver1)
+	{
+		this.driver= driver1;
+		
+	}
+
+	By Profile = By.xpath("/html/body/app-root/div/app-buyer-routing-layout/div/app-header/div[1]/div[2]/img");
+	By Username = By.xpath("/html/body/app-root/div/app-login/div/div[2]/form[1]/input");
+
+			//create user action
+	public void Profile_click ()
+	{
+		
+		System.out.println("gooo0000d");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.findElement(Profile).click();
+		System.out.println("clcik");
+	}
+	
+	
+	public void Send_username()
+	{
+		driver.findElement(Username).sendKeys("7892009925");
+		
+		
+		
+		
+	}
+	
+	
+	
+}
